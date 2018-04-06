de522c33d0a06575e905904c1b75818f2c7b51bc
Matthew Lam <matt@mryanlam.com>
2018-04-06 00:20:28 -0700

    merge in changes we had made previously

--- Antorus/argus-reloe-sender.lua
+++ Antorus/argus-reloe-sender.lua
@@ -20,12 +20,14 @@ aura_env.content = {
     -- phase 1 start is relative to ENCOUNTER_START event, which is 0 on logs/videos etc.
     {7, 5, "Melee Spread", 1},
     {26, 6, "Cone", 1},
+    {36, 5, "Gifts Inc", 1},
     {42, 5, "Melee Spread", 1},
     {47, 6, "Cone", 1},
-    {60, 3, "POTS", 1},
+    {60, 3, "2nd POTS", 1},
+    {64, 3, "Gifts Inc BAIT B4 MOVING", 1},
     {67, 6, "Cone", 1},
     {73, 9, "Buff -> Spread", 1},
-    {87, 6, "Cone", 1},
+    {87, 6, "Cone and DEFENSIVE", 1},
     {107, 6, "Cone", 1},
     
     -- phase 2 start is relative to the first Avatar being applied
@@ -40,19 +42,27 @@ aura_env.content = {
     {24, 6, "Bait", 3, true},
     {31, 5, "Potion", 3},
     {49, 4, "Chains inc", 3},
+    {52, 3, "Break BOTH CHAINS", 3}, -- First Chains Applied
     {72, 6, "Bait", 3, true},
     {93, 5, "Spread", 3},
-    {101, 4, "Die", 3},
-    {109, 5, "Release", 3},
+    {97, 3, "Suicide next CHAINS", 3}, -- 15second Suicide Warning
+    ---{101, 4, "Die", 3},
+    ---{109, 5, "Release", 3},
+    {111, 3, "SUICIDE", 3}, -- Suicide during EoA cast + Chains #2
     {115, 3, "Rune", 3},
     {119, 6, "Bait", 3, true},
+    {137, 3, "EoA Next (Rotate Raid)", 3}, -- 10sec warning before EoA   
     {164, 3, "Spread", 3},
-    {167, 6, "Bait", 3, true},
+    {167, 3, "Break AFTER Bait", 3}, -- Chains #3 Applied
+    {175, 3, "BREAK BOTH ASAP!!", 3}, -- Break BOTH #3 Chains in time before TR
     {216, 4, "Spread", 3},
-    {220, 6, "Bait", 3, true},
-    {226, 4, "Potion", 3},
+    {220, 3, "Break AFTER Bait", 3}, -- Chains #4 Applied
+    ---{226, 4, "Potion", 3},
+    {227, 3, "BREAK BOTH ASAP!!", 3}, -- Break BOTH #4 Chains in time before TR
+    {255, 4, "Lust!", 3},
     {269, 4, "Spread", 3},
     {273, 6, "Bait", 3, true},
+    {280, 3, "IGNORE MODULE, NUKE BOSS", 3},
 }
 
 for i=1,#aura_env.content do
